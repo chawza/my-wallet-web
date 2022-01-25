60ed78dcbc43e4a2f00e75914d8b7d64698a13f6
NabeelKM <nabeelkahlil403@gmail.com>
2022-01-25 11:48:48 +0700

    first commit: create-react-app + babel ES6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
